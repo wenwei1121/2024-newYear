53a9deddf350cede0bbf00d2f9c72b03bd435057
wenwei1121 <wenweiyang1121@gmail.com>
2024-02-04 23:27:15 +0800

    daisyUI table

--- src/components/DisplayMemberSort.vue
+++ src/components/DisplayMemberSort.vue
@@ -1,7 +1,30 @@
-<script setup lang="ts">
+<script setup lang="ts" generic="T extends { id: number }">
+import { computed } from "vue"
+
+interface Props {
+    fields: string[],
+    dataList: T[]
+}
+const props = defineProps<Props>()
+
+const slotDataList = computed(() => props.dataList);
 </script>
 
 <template>
+    <div class="overflow-x-auto">
+        <table class="table table-zebra">
+            <thead>
+                <tr>
+                    <th v-for="(field, _index) of fields" :key="_index">{{ field }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="data of slotDataList" :key="data.id">
+                    <slot :data="data" />
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <style>
